99ea31be22268a6948da3f08d629c2aed763a5bb
AishwaryaRK <aishwarya.kaneri@gmail.com>
2015-07-29 12:26:22 +0530

    chess plass prototype added

--- Chess.java
+++ Chess.java
@@ -1,3 +1,34 @@
-class Chess
-{
+class Chess {
+	private Cell[][] grid;
+
+	public Chess() {
+		this.grid = new Cell[8][8];
+		initializeGrid();
+	}
+
+	private void initializeGrid() {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void nextMove(String move, boolean playerFlag) {
+		// call putPiece
+		// call locateRemoveMovingPiece
+	}
+
+	private void putPiece(Piece piece, int row, int col) {
+		// TODO Auto-generated method stub
+
+	}
+
+	private void locateRemoveMovingPiece(Piece piece, int row, int col, boolean playerFlag, int positionIndex) {
+		// TODO Auto-generated method stub
+		// call removePiece
+	}
+
+	private void removePiece(int row, int col) {
+		// TODO Auto-generated method stub
+
+	}
+
 }
