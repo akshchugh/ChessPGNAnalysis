ed48f72062e40c5c31143581ff3469717161d914
AishwaryaRK <aishwarya.kaneri@gmail.com>
2015-07-30 09:42:44 +0530

    bugs in pawn func fixed

--- Client.java
+++ Client.java
@@ -1,9 +1,19 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
 public class Client {
 
-	public static void main(String[] args) {
-		Chess chess=new Chess();
-		chess.nextMove("Ke4", 'W');
+	public static void main(String[] args) throws FileNotFoundException {
+		File file = new File("C:/Training/ChessGame/src/input.txt");
+		Scanner scanner = new Scanner(file);
+		Chess chess = new Chess();
+		while (scanner.hasNextLine()) {
+			String string = scanner.nextLine();
+			String[] s = string.split(" ");
+			chess.nextMove(s[0], 'W');
+			chess.nextMove(s[1], 'B');
+		}
 		chess.displayBoard();
 	}
 
